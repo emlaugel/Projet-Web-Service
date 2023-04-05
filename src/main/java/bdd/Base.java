34fa36a00459b385f7f4b3ba7e181adb21be07e7
emlaugel <63586945+emlaugel@users.noreply.github.com>
2023-04-05 20:45:42 +0200

    Add files via upload

--- src/main/java/bdd/Base.java
+++ src/main/java/bdd/Base.java
@@ -14,7 +14,8 @@ public class Base
 	protected ResultSet resultat = null;
 	protected String id = "root";
 	protected String password = "Plume2002!";
-	protected String url = "jdbc:mysql://localhost:3306/javaee";
+	// CREATE DATABASE manag_agile
+	protected String url = "jdbc:mysql://localhost:3306/manag_agile";
 	// Pour avoir le port de MySQL (3306), entrez la commande :
 	//							show global variables like 'port'
 	
@@ -44,21 +45,21 @@ public class Base
 		{
 			statement = connexion.createStatement();
 			
-			String temp = "CREATE TABLE MUSIQUES (nom varchar(200) NOT NULL, album varchar(200),"
-												+ "duree varchar(200), sortie varchar(200), "
-												+ "artiste varchar(200), genre varchar(200),"
-												+ "lyrics varchar(8000));";
+			String temp = "CREATE TABLE PRODUIT (nom varchar(200) NOT NULL,"
+												+ "prix varchar(200),"
+												+ "type varchar(200),"
+												+ "contenance varchar(200),"
+												+ "marque varchar(200),"
+												+ "couleur varchar(8000),"
+												+ "support varchar(200));";
 			statement.executeUpdate(temp);
-			temp = "CREATE TABLE ARTISTES (nom_alternatif varchar(200) NOT NULL,"
-										+ "nom_reel varchar(200),"
-										+ "type varchar(200), nationalite varchar(200),"
-										+ "sexe varchar(200), naissance varchar(200),"
-										+ "status varchar(200), debut varchar(200),"
-										+ "genres_musicaux varchar(200), biographie varchar(3000));";
+			temp = "CREATE TABLE USERS (nom varchar(200) NOT NULL,"
+										+ "mdp varchar(200),"
+										+ "type varchar(200),"
+										+ "naissance varchar(200));";
 			
 			statement.executeUpdate(temp);
-			temp = "CREATE TABLE GENRES (nom varchar(200) NOT NULL, infos varchar(8000));";
-			statement.executeUpdate(temp);
+			
 			// varchar peut aller jusqu'a 65535 octets depuis MySQL 5.5
 			
 			retour = "Base de données créée";
@@ -76,150 +77,22 @@ public class Base
 		return retour;
 	}
 	
-	public void addMusique(String nom, String album, String duree, String sortie,String artiste, String genre, String lyrics)
+	public void addProduit(String nom, String prix, String type, String contenance,String marque, String couleur, String support)
 	{
 		loaddatabase();
 		try 
 		{
 			statement = connexion.createStatement();
 			
-			String temp = "INSERT INTO MUSIQUES(nom, album, duree, sortie, artiste, genre, lyrics) VALUES(?, ?, ?, ?, ?, ?, ?);";
+			String temp = "INSERT INTO PRODUIT(nom, prix, type, contenance, marque, couleur, support) VALUES(?, ?, ?, ?, ?, ?, ?);";
 			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
 			preparedStatement.setString(1, nom);
-			preparedStatement.setString(2, album);
-			preparedStatement.setString(3, duree);
-			preparedStatement.setString(4, sortie);
-			preparedStatement.setString(5, artiste);
-			preparedStatement.setString(6, genre);
-			preparedStatement.setString(7, lyrics);
-			
-			preparedStatement.executeUpdate();
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public void delMusique(String nom)
-	{
-		loaddatabase();
-		try 
-		{
-			statement = connexion.createStatement();
-			
-			String temp = "DELETE FROM musiques WHERE nom = ?;";
-			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
-			preparedStatement.setString(1, nom);
-			
-	        preparedStatement.executeUpdate();
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public List<String> recupererMusiques()
-	{
-		List<String> retour = new ArrayList<String>();
-		loaddatabase();
-		
-		try
-		{
-			statement = connexion.createStatement();
-			resultat = statement.executeQuery("SELECT nom FROM MUSIQUES;");
-			while (resultat.next())
-			{
-				String musique = resultat.getString("nom");
-				retour.add(musique);
-			}
-			
-		}
-		catch (SQLException e)
-		{
-			try
-			{
-				if (resultat != null) { resultat.close(); }
-				if (resultat != null) { statement.close(); }
-				if (resultat != null) { connexion.close(); }
-			}
-			catch (SQLException ignore) {}
-		}
-		return retour;
-	}
-
-	public List<String> infoMusique(String nom)
-	{
-		List<String> retour = new ArrayList<String>();
-		loaddatabase();
-
-		try
-		{			
-			String temp = "SELECT * FROM musiques WHERE nom = ?;";
-			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
-			preparedStatement.setString(1, nom);
-			
-	        resultat = preparedStatement.executeQuery();
-			
-	        while (resultat.next()) {
-				String album = resultat.getString("album");
-				retour.add(album);
-				
-				String duree = resultat.getString("duree");
-				retour.add(duree);
-				
-				String sortie =  resultat.getString("sortie");
-				retour.add(sortie);
-				
-				String artiste =  resultat.getString("artiste");
-				retour.add(artiste);
-				
-				String genre =  resultat.getString("genre");
-				retour.add(genre);
-				
-				String lyrics =  resultat.getString("lyrics");
-				retour.add(lyrics);
-	        }
-		}
-		catch (SQLException e)
-		{
-			try
-			{
-				if (resultat != null) { resultat.close(); }
-				if (resultat != null) { statement.close(); }
-				if (resultat != null) { connexion.close(); }
-			}
-			catch (SQLException ignore) {}
-			catch (NullPointerException ignore) {}
-		}
-		
-		return retour;
-	}
-	
-	
-	
-	public void addArtiste(String nom_alternatif, String nom_reel, String type, String nationalite, String sexe, String naissance, String status, String debut, String genres_musicaux, String biographie)
-	{	
-		
-		loaddatabase();
-		try 
-		{
-			statement = connexion.createStatement();
-			
-			String temp = "INSERT INTO ARTISTES(nom_alternatif, nom_reel, type, nationalite, sexe, naissance, status, debut, genres_musicaux, biographie) VALUES(?,?,?,?,?,?,?,?,?,?);";			
-			
-			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
-			preparedStatement.setString(1, nom_alternatif);
-			preparedStatement.setString(2, nom_reel);
+			preparedStatement.setString(2, prix);
 			preparedStatement.setString(3, type);
-			preparedStatement.setString(4, nationalite);
-			preparedStatement.setString(5, sexe);
-			preparedStatement.setString(6, naissance);
-			preparedStatement.setString(7, status);
-			preparedStatement.setString(8, debut);
-			preparedStatement.setString(9, genres_musicaux);
-			preparedStatement.setString(10, biographie);
+			preparedStatement.setString(4, contenance);
+			preparedStatement.setString(5, marque);
+			preparedStatement.setString(6, couleur);
+			preparedStatement.setString(7, support);
 			
 			preparedStatement.executeUpdate();
 		}
@@ -228,17 +101,17 @@ public class Base
 			e.printStackTrace();
 		}
 	}
-
-	public void delArtiste(String nom_alternatif)
+	
+	public void delProduit(String nom)
 	{
 		loaddatabase();
 		try 
 		{
 			statement = connexion.createStatement();
 			
-			String temp = "DELETE FROM artistes WHERE nom_alternatif = ?;";
+			String temp = "DELETE FROM PRODUIT WHERE nom = ?;";
 			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
-			preparedStatement.setString(1, nom_alternatif);
+			preparedStatement.setString(1, nom);
 			
 	        preparedStatement.executeUpdate();
 		}
@@ -248,7 +121,7 @@ public class Base
 		}
 	}
 	
-	public List<String> recupererArtistes()
+	public List<String> recupererProduits()
 	{
 		List<String> retour = new ArrayList<String>();
 		loaddatabase();
@@ -256,13 +129,12 @@ public class Base
 		try
 		{
 			statement = connexion.createStatement();
-			resultat = statement.executeQuery("SELECT nom_alternatif FROM ARTISTES;");
+			resultat = statement.executeQuery("SELECT nom FROM PRODUIT;");
 			while (resultat.next())
 			{
-				String artiste = resultat.getString("nom_alternatif");
-				retour.add(artiste);
+				String produit = resultat.getString("nom");
+				retour.add(produit);
 			}
-			
 		}
 		catch (SQLException e)
 		{
@@ -274,50 +146,40 @@ public class Base
 			}
 			catch (SQLException ignore) {}
 		}
-		
 		return retour;
 	}
-	
-	public List<String> infoArtiste(String nom_alternatif)
+
+	public List<String> infoProduit(String nom)
 	{
 		List<String> retour = new ArrayList<String>();
 		loaddatabase();
 
 		try
 		{			
-			String temp = "SELECT * FROM artistes WHERE nom_alternatif = ?;";
+			String temp = "SELECT * FROM PRODUIT WHERE nom = ?;";
 			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
-			preparedStatement.setString(1, nom_alternatif);
+			preparedStatement.setString(1, nom);
 			
 	        resultat = preparedStatement.executeQuery();
 			
 	        while (resultat.next()) {
-				String nom_reel = resultat.getString("nom_reel");
-				retour.add(nom_reel);
+				String prix = resultat.getString("prix");
+				retour.add(prix);
 				
 				String type = resultat.getString("type");
 				retour.add(type);
 				
-				String nationalite =  resultat.getString("nationalite");
-				retour.add(nationalite);
+				String contenance =  resultat.getString("contenance");
+				retour.add(contenance);
 				
-				String sexe =  resultat.getString("sexe");
-				retour.add(sexe);
+				String marque =  resultat.getString("marque");
+				retour.add(marque);
 				
-				String naissance =  resultat.getString("naissance");
-				retour.add(naissance);
+				String couleur =  resultat.getString("couleur");
+				retour.add(couleur);
 				
-				String status =  resultat.getString("status");
-				retour.add(status);
-				
-				String debut =  resultat.getString("debut");
-				retour.add(debut);
-				
-				String genres_musicaux =  resultat.getString("genres_musicaux");
-				retour.add(genres_musicaux);
-				
-				String biographie =  resultat.getString("biographie");
-				retour.add(biographie);
+				String support =  resultat.getString("support");
+				retour.add(support);
 	        }
 		}
 		catch (SQLException e)
@@ -334,20 +196,22 @@ public class Base
 		
 		return retour;
 	}
-	
 
-	
-	public void addGenre(String nom, String infos)
+	public void addUser(String nom, String mdp, String type, String naissance)
 	{	
+		
 		loaddatabase();
 		try 
 		{
 			statement = connexion.createStatement();
 			
-			String temp = "INSERT INTO GENRES(nom, infos) VALUES(?, ?);";
+			String temp = "INSERT INTO USERS(nom, mdp, type, naissance) VALUES(?,?,?,?);";			
+			
 			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
 			preparedStatement.setString(1, nom);
-			preparedStatement.setString(2, infos);
+			preparedStatement.setString(2, mdp);
+			preparedStatement.setString(3, type);
+			preparedStatement.setString(4, naissance);
 			
 			preparedStatement.executeUpdate();
 		}
@@ -356,15 +220,15 @@ public class Base
 			e.printStackTrace();
 		}
 	}
-	
-	public void delGenre(String nom)
+
+	public void delUser(String nom)
 	{
 		loaddatabase();
 		try 
 		{
 			statement = connexion.createStatement();
 			
-			String temp = "DELETE FROM genres WHERE nom = ?;";
+			String temp = "DELETE FROM USERS WHERE nom = ?;";
 			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
 			preparedStatement.setString(1, nom);
 			
@@ -376,7 +240,7 @@ public class Base
 		}
 	}
 	
-	public List<String> recupererGenres()
+	public List<String> recupererUsers()
 	{
 		List<String> retour = new ArrayList<String>();
 		loaddatabase();
@@ -384,11 +248,11 @@ public class Base
 		try
 		{
 			statement = connexion.createStatement();
-			resultat = statement.executeQuery("SELECT nom FROM GENRES;");
+			resultat = statement.executeQuery("SELECT nom FROM USERS;");
 			while (resultat.next())
 			{
-				String artiste = resultat.getString("nom");
-				retour.add(artiste);
+				String nom = resultat.getString("nom");
+				retour.add(nom);
 			}
 			
 		}
@@ -406,22 +270,28 @@ public class Base
 		return retour;
 	}
 	
-	public List<String> infoGenre(String nom)
+	public List<String> infoUsers(String nom)
 	{
 		List<String> retour = new ArrayList<String>();
 		loaddatabase();
 
 		try
 		{			
-			String temp = "SELECT * FROM genres WHERE nom = ?;";
+			String temp = "SELECT * FROM USERS WHERE nom = ?;";
 			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
 			preparedStatement.setString(1, nom);
 			
 	        resultat = preparedStatement.executeQuery();
 			
 	        while (resultat.next()) {
-				String infos = resultat.getString("infos");
-				retour.add(infos);
+				String mdp = resultat.getString("mdp");
+				retour.add(mdp);
+				
+				String type = resultat.getString("type");
+				retour.add(type);
+				
+				String naissance =  resultat.getString("naissance");
+				retour.add(naissance);
 	        }
 		}
 		catch (SQLException e)
@@ -438,7 +308,7 @@ public class Base
 		
 		return retour;
 	}
-	
+
 	public List<String> rechercher(String valeur) throws EncoderException
 	{
 		List<String> retour = new ArrayList<String>();
@@ -449,7 +319,7 @@ public class Base
 
 		try
 		{	
-			String temp = "SELECT * FROM artistes;";
+			String temp = "SELECT * FROM PRODUIT;";
 			PreparedStatement preparedStatement = connexion.prepareStatement(temp);
 			
 	        resultat = preparedStatement.executeQuery();
@@ -458,18 +328,18 @@ public class Base
 	        	
 	        	compare_temp.clear();
 	        	
-				String nom_alternatif = resultat.getString("nom_alternatif");
-				compare_temp.add(nom_alternatif);
+				String nom = resultat.getString("nom");
+				compare_temp.add(nom);
 				
 				for (String s : compare_temp) {
-					if (retour.contains(nom_alternatif) == false)
+					if (retour.contains(nom) == false)
 					{
-						retour.add(soundex.difference(s, valeur) + "=" + nom_alternatif + "=" + "artiste");
+						retour.add(soundex.difference(s, valeur) + "=" + nom + "=" + "nom_produit");
 					}
 	        	}
 	        }
 	        
-	        //
+	        /*
 	        
 	        temp = "SELECT * FROM genres;";
 			preparedStatement = connexion.prepareStatement(temp);
@@ -490,26 +360,7 @@ public class Base
 	        	}
 	        }
 	        
-	        //
-	        
-	        temp = "SELECT * FROM musiques;";
-			preparedStatement = connexion.prepareStatement(temp);
-			
-	        resultat = preparedStatement.executeQuery();
-	        
-	        while (resultat.next()) {
-	        	compare_temp.clear();
-
-	        	String nom = resultat.getString("nom");
-	        	compare_temp.add(nom);
-				
-				for (String s : compare_temp) {
-					if (!retour.contains(nom))
-					{
-						retour.add(soundex.difference(s, valeur) + "=" + nom + "=" + "musique");
-					}
-	        	}
-	        }
+	        */
 		}
 		catch (SQLException e)
 		{
